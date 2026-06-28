a6074fbb496a3e9eac9aa1ef82d4a5fb36f82964
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2026-06-28 17:18:31 -0300

    Implement application model, repository, and service layers for application management

---
+++ internal/application/service.go
@@ -0,0 +1,66 @@
+package application
+
+import (
+	"context"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+type Service struct {
+	r ApplicationRepository
+}
+
+func NewService(r ApplicationRepository) *Service {
+	return &Service{r: r}
+}
+
+func (s *Service) Create(ctx context.Context, req CreateApplication) (*Application, error) {
+
+	app, err := s.r.Create(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return app, nil
+}
+
+func (s *Service) FindAll(ctx context.Context) ([]Application, error) {
+	apps, err := s.r.FindAll(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return apps, nil
+}
+
+func (s *Service) FindByID(ctx context.Context, id uuid.UUID) (*Application, error) {
+	app, err := s.r.FindByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+
+	if app == nil {
+		return nil, errors.New("application not found")
+	}
+
+	return app, nil
+}
+
+func (s *Service) Update(ctx context.Context, id uuid.UUID, req UpdateApplication) (*Application, error) {
+	app, err := s.r.Update(ctx, id, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return app, nil
+}
+
+func (s *Service) Delete(ctx context.Context, id uuid.UUID) error {
+	err := s.r.Delete(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
